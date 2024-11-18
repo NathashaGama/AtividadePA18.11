e8c5e1419a6654245da9f9c57cd12049b2830563
Nathasha Gama <nathashagama@gmail.com>
2024-11-18 19:27:09 -0300

    first commit

---
+++ AtividadeSaiz18.11/MainForm.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * Criado por SharpDevelop.
+ * Usuário: aluno
+ * Data: 18/11/2024
+ * Hora: 19:20
+ * 
+ * Para alterar este modelo use Ferramentas | Opções | Codificação | Editar Cabeçalhos Padrão.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace atividadeSaiz18._1
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+	}
+}
