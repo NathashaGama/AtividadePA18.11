91d5dbcacc58f7fcdb1d6b43ab5c07c601fd4a98
Nathasha Gama <nathashagama@gmail.com>
2024-11-22 00:34:19 -0300

    atualizacao5

--- AtividadeSaiz18.11/MainForm.Designer.cs
+++ AtividadeSaiz18.11/MainForm.Designer.cs
@@ -28,7 +28,6 @@ namespace atividadeSaiz18._1
 		private System.Windows.Forms.Button button5;
 		private System.Windows.Forms.RichTextBox richTextBox2;
 		private System.Windows.Forms.Button button6;
-		private System.Windows.Forms.Button button7;
 		
 		/// <summary>
 		/// Disposes resources used by the form.
@@ -65,12 +64,10 @@ namespace atividadeSaiz18._1
 			this.button5 = new System.Windows.Forms.Button();
 			this.richTextBox2 = new System.Windows.Forms.RichTextBox();
 			this.button6 = new System.Windows.Forms.Button();
-			this.button7 = new System.Windows.Forms.Button();
 			this.textBox4 = new System.Windows.Forms.TextBox();
 			this.textBox5 = new System.Windows.Forms.TextBox();
 			this.label4 = new System.Windows.Forms.Label();
 			this.label5 = new System.Windows.Forms.Label();
-			this.button8 = new System.Windows.Forms.Button();
 			this.SuspendLayout();
 			// 
 			// richTextBox1
@@ -87,12 +84,12 @@ namespace atividadeSaiz18._1
 			// 
 			this.button1.BackColor = System.Drawing.Color.NavajoWhite;
 			this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.button1.Location = new System.Drawing.Point(188, 354);
+			this.button1.Location = new System.Drawing.Point(17, 197);
 			this.button1.Margin = new System.Windows.Forms.Padding(2);
 			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(63, 35);
+			this.button1.Size = new System.Drawing.Size(105, 49);
 			this.button1.TabIndex = 1;
-			this.button1.Text = "Salvar";
+			this.button1.Text = "ADD>>";
 			this.button1.UseVisualStyleBackColor = false;
 			this.button1.Click += new System.EventHandler(this.Button1Click);
 			// 
@@ -100,12 +97,12 @@ namespace atividadeSaiz18._1
 			// 
 			this.button2.BackColor = System.Drawing.Color.NavajoWhite;
 			this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.button2.Location = new System.Drawing.Point(255, 354);
+			this.button2.Location = new System.Drawing.Point(188, 354);
 			this.button2.Margin = new System.Windows.Forms.Padding(2);
 			this.button2.Name = "button2";
 			this.button2.Size = new System.Drawing.Size(63, 35);
 			this.button2.TabIndex = 2;
-			this.button2.Text = "Abrir";
+			this.button2.Text = "Salvar";
 			this.button2.UseVisualStyleBackColor = false;
 			this.button2.Click += new System.EventHandler(this.Button2Click);
 			// 
@@ -113,12 +110,12 @@ namespace atividadeSaiz18._1
 			// 
 			this.button3.BackColor = System.Drawing.Color.NavajoWhite;
 			this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.button3.Location = new System.Drawing.Point(324, 354);
+			this.button3.Location = new System.Drawing.Point(255, 354);
 			this.button3.Margin = new System.Windows.Forms.Padding(2);
 			this.button3.Name = "button3";
 			this.button3.Size = new System.Drawing.Size(63, 35);
 			this.button3.TabIndex = 3;
-			this.button3.Text = "Limpar";
+			this.button3.Text = "Abrir";
 			this.button3.UseVisualStyleBackColor = false;
 			this.button3.Click += new System.EventHandler(this.Button3Click);
 			// 
@@ -126,12 +123,12 @@ namespace atividadeSaiz18._1
 			// 
 			this.button4.BackColor = System.Drawing.Color.NavajoWhite;
 			this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.button4.Location = new System.Drawing.Point(17, 207);
+			this.button4.Location = new System.Drawing.Point(322, 354);
 			this.button4.Margin = new System.Windows.Forms.Padding(2);
 			this.button4.Name = "button4";
-			this.button4.Size = new System.Drawing.Size(104, 35);
+			this.button4.Size = new System.Drawing.Size(65, 35);
 			this.button4.TabIndex = 4;
-			this.button4.Text = "ADD>>";
+			this.button4.Text = "Limpar";
 			this.button4.UseVisualStyleBackColor = false;
 			this.button4.Click += new System.EventHandler(this.Button4Click);
 			// 
@@ -196,12 +193,12 @@ namespace atividadeSaiz18._1
 			// 
 			this.button5.BackColor = System.Drawing.Color.NavajoWhite;
 			this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.button5.Location = new System.Drawing.Point(17, 366);
+			this.button5.Location = new System.Drawing.Point(414, 354);
 			this.button5.Margin = new System.Windows.Forms.Padding(2);
 			this.button5.Name = "button5";
-			this.button5.Size = new System.Drawing.Size(104, 35);
+			this.button5.Size = new System.Drawing.Size(199, 35);
 			this.button5.TabIndex = 13;
-			this.button5.Text = "Calcular";
+			this.button5.Text = "Mostrar Gastos";
 			this.button5.UseVisualStyleBackColor = false;
 			this.button5.Click += new System.EventHandler(this.Button5Click);
 			// 
@@ -219,27 +216,14 @@ namespace atividadeSaiz18._1
 			// 
 			this.button6.BackColor = System.Drawing.Color.NavajoWhite;
 			this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.button6.Location = new System.Drawing.Point(414, 354);
+			this.button6.Location = new System.Drawing.Point(17, 366);
 			this.button6.Margin = new System.Windows.Forms.Padding(2);
 			this.button6.Name = "button6";
-			this.button6.Size = new System.Drawing.Size(71, 35);
+			this.button6.Size = new System.Drawing.Size(100, 35);
 			this.button6.TabIndex = 15;
-			this.button6.Text = "Salvar";
+			this.button6.Text = "Calcular";
 			this.button6.UseVisualStyleBackColor = false;
 			this.button6.Click += new System.EventHandler(this.Button6Click);
-			// 
-			// button7
-			// 
-			this.button7.BackColor = System.Drawing.Color.NavajoWhite;
-			this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.button7.Location = new System.Drawing.Point(545, 354);
-			this.button7.Margin = new System.Windows.Forms.Padding(2);
-			this.button7.Name = "button7";
-			this.button7.Size = new System.Drawing.Size(68, 35);
-			this.button7.TabIndex = 16;
-			this.button7.Text = "Limpar";
-			this.button7.UseVisualStyleBackColor = false;
-			this.button7.Click += new System.EventHandler(this.Button7Click);
 			// 
 			// textBox4
 			// 
@@ -275,31 +259,16 @@ namespace atividadeSaiz18._1
 			this.label5.TabIndex = 20;
 			this.label5.Text = "Valor Médio";
 			// 
-			// button8
-			// 
-			this.button8.BackColor = System.Drawing.Color.NavajoWhite;
-			this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.button8.Location = new System.Drawing.Point(478, 354);
-			this.button8.Margin = new System.Windows.Forms.Padding(2);
-			this.button8.Name = "button8";
-			this.button8.Size = new System.Drawing.Size(63, 35);
-			this.button8.TabIndex = 21;
-			this.button8.Text = "Abrir";
-			this.button8.UseVisualStyleBackColor = false;
-			this.button8.Click += new System.EventHandler(this.Button8Click);
-			// 
 			// MainForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.BackColor = System.Drawing.Color.Cornsilk;
 			this.ClientSize = new System.Drawing.Size(826, 412);
-			this.Controls.Add(this.button8);
 			this.Controls.Add(this.label5);
 			this.Controls.Add(this.label4);
 			this.Controls.Add(this.textBox5);
 			this.Controls.Add(this.textBox4);
-			this.Controls.Add(this.button7);
 			this.Controls.Add(this.button6);
 			this.Controls.Add(this.richTextBox2);
 			this.Controls.Add(this.button5);
@@ -320,7 +289,6 @@ namespace atividadeSaiz18._1
 			this.ResumeLayout(false);
 			this.PerformLayout();
 		}
-		private System.Windows.Forms.Button button8;
 		private System.Windows.Forms.Label label5;
 		private System.Windows.Forms.Label label4;
 		private System.Windows.Forms.TextBox textBox5;
--- AtividadeSaiz18.11/MainForm.cs
+++ AtividadeSaiz18.11/MainForm.cs
@@ -29,144 +29,108 @@ namespace atividadeSaiz18._1
 			// TODO: Add constructor code after the InitializeComponent() call.
 			//
 		}
-		void Button4Click(object sender, EventArgs e)
-		{
-	// adicionar 
-	richTextBox1.Text += textBox1.Text + " | " + textBox2.Text + " | " + textBox3.Text + "\n";
-	textBox1.Clear();
-	textBox2.Clear();
-	textBox3.Clear();
-		}
+		
 		void Button1Click(object sender, EventArgs e)
 		{
-	//Botão de salvar
-	richTextBox1.SaveFile("funcionarios.txt", RichTextBoxStreamType.PlainText);
-	MessageBox.Show("Arquivo salvo com sucesso");
-		}
-		void Button2Click(object sender, EventArgs e)
-		{
-	//Abrir
-	richTextBox1.LoadFile("funcionarios.txt", RichTextBoxStreamType.PlainText);
-		}
-		void Button3Click(object sender, EventArgs e)
-		{
-	//Limpar 
-	richTextBox1.Clear();
-		}
-		void Button6Click(object sender, EventArgs e)
-		{
-			//2 botão de salvar
-			richTextBox2.SaveFile("gastos.txt", RichTextBoxStreamType.PlainText);
-			MessageBox.Show("Arquivo salvo com sucesso");
+			richTextBox1.Text += textBox1.Text + "|" + textBox2.Text + "|"+ textBox3.Text + "\n";
+			textBox1.Clear();
+			textBox2.Clear();
+			textBox3.Clear();
 		}
 		
-		void Button7Click(object sender, EventArgs e)
+		void Button2Click(object sender, EventArgs e)
 		{
-			//Limpar segundo richBox
-	richTextBox2.Clear();
+			richTextBox1.SaveFile("funcionários.txt", RichTextBoxStreamType. PlainText);
+		}
+		
+		void Button3Click(object sender, EventArgs e)
+		{
+			  richTextBox1.LoadFile("funcionários.txt", RichTextBoxStreamType.PlainText);
+		}
+		
+		void Button4Click(object sender, EventArgs e)
+		{
+			richTextBox1.Clear();
+		}
+		
+		void Button6Click(object sender, EventArgs e)
+		{
+			int somaTotal =0;
+			int contador = 0;
+			
+			string[] linhas = richTextBox1.Lines;
+			
+			for (int i=0; i < linhas.Length; i++)
+			{
+				if (string.IsNullOrWhiteSpace(linhas[i]))
+				continue;
+				
+				string[] partes= linhas[2].Split('|');
+				if (partes.Length >= 3)
+				{
+					try
+					{
+						int valor= int.Parse(partes [2].Trim());
+						
+						somaTotal+= valor;
+						contador++;
+					}
+					catch
+					  {
+					  	MessageBox.Show("Erro ao processar o valor na linha: {linhas[i]}");
+					  }
+				
+				}else
+				  {
+				 	MessageBox.Show("Linha inválida encontrada: {linhas[i]}"); 
+				  }
+			}
+				textBox4.Text= somaTotal.ToString("C"); 
+			
+				int media= contador > 0 ? somaTotal / contador: 0;
+				textBox5.Text = media.ToString("C"); 
 		}
 		
 		void Button5Click(object sender, EventArgs e)
 		{
-		// soma do total
-			double total= 0;
-			int cont= 0;
-		// pegar linha 
-			string[] linhas = richTextBox1.Lines;
+			string[] linhas= richTextBox1.Lines;
+
+			int media;
+			string mediaText= textBox5.Text.Replace("R$","").Trim();
 			
-			for (int i= 0; i<linhas.Length; i++){
-		//tirar linhas sem dados
-			if(string.IsNullOrWhiteSpace(linhas[i])) continue;
-	
-		//pegar dado
-			string[] dado1= linhas[i].Split('|');
-	
-			if (dado1.Length>=3)
-			{
-				try
-				{
-					double valor= double.Parse(dado1[1].Trim());
-					total += valor;
-					cont++; 
-				}
-				catch
-				{
-					// pra o caso de dar errado
-					MessageBox.Show("Erro ao processar o valor na linha {i=1}: {linhas[i]}");
-				}
-			}else
-			{
-				MessageBox.Show("Linha inválida encontrada: {limhas[i]}");
+			if (!int. TryParse(textBox5.Text.Trim(), out media))
+			 {
+			  MessageBox.Show("Por favor, calcule a média antes de usar este botão.");
+			  return;
 			}
-}
-			textBox4.Text= total.ToString("C");
+			string dadosAcimaMedia= "";
 			
-			//calculo da média
-			double media= cont>0 ? total/cont : 0;
-			
-			// exibir
-			textBox5.Text= media.ToString("C");
-			}
-	
-		
-		void Button8Click(object sender, EventArgs e)
-		{
-			//Obter todas as linhas do RichTextBox
-
-			string[] linhas=richTextBox1.Lines;
-
-			// Obter a média calculada do TextBox (Certifique-se de que ela esteja em formato numërico)
-
-			double media;
-
-			if (!double.TryParse(textBox5.Text.Trim(), out media))
-			{
-			MessageBox.Show("Por favor, calcule a média antes de usar este botão.");
-			return;
-			}
-
-			// Variável para armazenar os dados acima da média
-
-			string dadosAcimalMedia = "";
-
-			// Loop para verificar quais dados estão acima de média 
 			for (int i=0; i < linhas.Length; i++)
-			{
-			// Ignorar linhas vazias
-
-				if (string.IsNullOrwhiteSpace(linhas[i]))
-				continue;
-	
-				// Dividir a linha em partes 
-				string[] partes=linhas[i].Split(',');
-				
-				// Garantir que a linha possui pelo menos 3 partes (Nome, Telefone, Valor) 
-				if (partes.Length >= 3)
-				{
-				
-					try
-					{
-						// Tentar converter o valor da conta para double 
-						double valor=double.Parse(partes [2].Trim());
-				
-						// Verificar se o valor ultrapassa a média
-						if (valor > media)
-						{
-						// Adicionar a linha ao resultado
-						dadosAcimaMedia += linhas[i]+Environment.NewLine;
-						}
-					}
-						catch
-						{
-							MessageBox.Show("Erro ao processar o valor");
-						}
-				 }
-			}
-				richTextBox2.Text= dadosAcimalMedia;
-				if(string.IsNullOrWhiteSpace(dadosAcimalMedia))
-				{
-					MessageBox.Show("Nenhum dado está acima da média");
-				}
-	 }
-	} }
-	
+			  {
+			  if (string.IsNullOrWhiteSpace(linhas[i]))
+			continue;
+			
+			  string[] partes=linhas[i].Split(',');
+			
+			if (partes.Length >= 3)
+			   {
+			   try
+			    {
+			   	int valor= int.Parse(partes[2].Trim());
+			   	if (valor>media)
+			   	{
+			   		dadosAcimaMedia += linhas[i] + Environment. NewLine;
+			     }
+			
+			    }
+			    catch
+			     {
+			      MessageBox.Show("Erro ao processar o valor na linha {i+1}: {linhas[i]}");
+			     }
+			
+			   }
+			  }
+			richTextBox2.Text= dadosAcimaMedia;
+		}
+	}	
+}
